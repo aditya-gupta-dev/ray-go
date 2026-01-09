81e1b4b9c15ff3715c8ca8d8ebae7a2a6bdc33e9
Aditya Gupta <adityagupta2087@gmail.com>
2026-01-09 19:29:48 +0530

    added gitignore

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"ray-go/window"
+	"unsafe"
+)
+
+type LifeCycle struct{}
+
 func main() {
+
+	var window = window.Window{}
+
+	fmt.Println(unsafe.Sizeof(window))
 }
